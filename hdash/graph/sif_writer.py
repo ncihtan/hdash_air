ac5f6746318cb8935094eea3ad21c3bb4064c4ee
ecerami <cerami@gmail.com>
2023-04-04 08:11:06 -0400

    Migrate sif writer

---
+++ hdash/graph/sif_writer.py
@@ -0,0 +1,23 @@
+"""SIF Writer."""
+import networkx as nx
+from hdash.graph.htan_graph import HtanGraph
+
+
+class SifWriter:
+    """SIF Network Writer for Cytoscape."""
+
+    def __init__(self, graph: nx.DiGraph):
+        """Create Cytoscape SIF Writer."""
+        self.graph = graph
+        self.sif = ""
+        self._create_sif()
+
+    def _create_sif(self):
+        """Create the SIF Network."""
+        edge_list = self.graph.edges
+        for edge in edge_list:
+            node0_id = edge[0]
+            node1_id = edge[1]
+            sif_id0 = self.graph.nodes[node0_id][HtanGraph.DATA_KEY].sif_id
+            sif_id1 = self.graph.nodes[node1_id][HtanGraph.DATA_KEY].sif_id
+            self.sif += f"{sif_id0}\tconnect\t{sif_id1}\n"
---
+++ tests/test_sif_writer.py
@@ -0,0 +1,16 @@
+"""Test Graph Creator."""
+from hdash.graph.graph_creator import GraphCreator
+from hdash.graph.sif_writer import SifWriter
+
+
+def test_sif_writer(sample_meta_map):
+    """Test SIF Writer."""
+    meta_map = sample_meta_map
+    graph_creator = GraphCreator("HTA1", meta_map)
+    directed_graph = graph_creator.htan_graph.directed_graph
+    sif_writer = SifWriter(directed_graph)
+
+    # Check for Specific Edges
+    sif = sif_writer.sif
+    assert "B_HTA3_8001_002\tconnect\tSC1_HTA3_8001_4651918348" in sif
+    assert "B_HTA3_8001_001	connect	SC1_HTA3_8001_7837535366" in sif
