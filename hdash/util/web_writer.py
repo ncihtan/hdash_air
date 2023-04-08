373e4f1189cf7eefc7a9e753b255d1e893613145
ecerami <cerami@gmail.com>
2023-04-07 20:02:57 -0400

    Remove matrix page

--- hdash/util/web_writer.py
+++ hdash/util/web_writer.py
@@ -31,7 +31,6 @@ class WebWriter:
         self.now_str = self.now.strftime("%m/%d/%Y %H:%M:%S")
         self._generate_index_html()
         self._generate_atlas_pages()
-        #self._generate_matrix_pages()
 
     def _generate_index_html(self):
         template = self.env.get_template("index.html")
@@ -50,18 +49,6 @@ class WebWriter:
             template = self.env.get_template("atlas.html")
             html = template.render(now=self.now_str, atlas_info=atlas_info)
             self.atlas_html_map[atlas_info.info.atlas_id] = html
-
-    def _generate_matrix_pages(self):
-        for atlas_info in self.atlas_list:
-            current_map = {}
-            if len(atlas_info.meta_list) > 0:
-                for heatmap in atlas_info.matrix_list:
-                    template = self.env.get_template("matrix.html")
-                    html = template.render(
-                        now=self.now_str, atlas=atlas_info, heatmap=heatmap
-                    )
-                    current_map[heatmap.id] = html
-            self.matrix_html_map[atlas_info.info.atlas_id] = current_map
 
     def _get_template_env(self):
         return Environment(
