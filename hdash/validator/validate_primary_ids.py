13cb4009fb8f72afe7f78d33d139903183c0df7e
ecerami <cerami@gmail.com>
2024-04-11 11:27:19 -0400

    Added checks or redundant primary IDs.

--- hdash/validator/validate_primary_ids.py
+++ hdash/validator/validate_primary_ids.py
@@ -67,15 +67,15 @@ class ValidatePrimaryIds(ValidationRule):
 
     def __check_primary_id(self, meta_file: MetaFile, category, primary_id, atlas_id):
         primary_id = str(primary_id)
+        error = False
         if "," in primary_id:
             msg = "Primary ID can only have one ID:  " + primary_id
             self.add_error(msg, meta_file)
+            error = True
         elif primary_id == "nan":
             msg = "Primary ID is missing from specific row."
             self.add_error(msg, meta_file)
-        elif primary_id in self.primary_id_set:
-            msg = f"Primary ID {primary_id} has already been defined in {category}."
-            self.add_error(msg, meta_file)
+            error = True
         else:
             match = bool(re.match(
                 "^(HTA([1-9]|1[0-5]))_((EXT)?([0-9]\d*|0000))_([0-9]\d*|0000)$",
@@ -84,3 +84,13 @@ class ValidatePrimaryIds(ValidationRule):
             if not match:
                 msg = primary_id + " does not match HTAN spec."
                 self.add_error(msg, meta_file)
+                error = True
+
+        if not error:
+            if primary_id in self.primary_id_set:
+                (defined_synapse_id, defined_category) = self.primary_id_set[primary_id]
+                msg = f"Primary ID {primary_id} has already been defined in file {defined_synapse_id} of type {defined_category}."
+                self.add_error(msg, meta_file)
+            else:
+                synapse_id = meta_file.atlas_file.synapse_id
+                self.primary_id_set[primary_id] = (synapse_id, category)
