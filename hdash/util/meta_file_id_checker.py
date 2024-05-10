d678cda541f76b4a72510359112e6a57a89a4111
ecerami <cerami@gmail.com>
2024-05-09 22:13:09 -0400

    Add exception for accessory manifests and level2 data

--- hdash/util/meta_file_id_checker.py
+++ hdash/util/meta_file_id_checker.py
@@ -2,6 +2,7 @@
 import logging
 from hdash.util.id_util import IdUtil
 from hdash.util.id_checker import IdChecker
+from hdash.util.categories import Categories
 
 
 class MetaFileIdChecker:
@@ -14,6 +15,7 @@ class MetaFileIdChecker:
         self.error_list = []
         self.id_util = IdUtil()
         self.id_checker = IdChecker()
+        self.categories = Categories()
 
         self.synapse_id = synapse_id
         self.category = category
@@ -21,28 +23,29 @@ class MetaFileIdChecker:
         self.valid_primary_id_list = []
 
         # Validate Primary IDs
-        primary_id_col = self.id_util.get_primary_id_column(self.category)
-        id_list = self.data_frame[primary_id_col].to_list()
-        for current_id in id_list:
-            current_id = str(current_id)
-            if not self.id_checker.is_valid_htan_id(primary_id_col, current_id):
-                msg = f"Invalid {primary_id_col}:  {current_id}"
-                msg = self._create_error_msg(msg)
-                self.error_list.append(msg)
-            else:
-                self.valid_primary_id_list.append(current_id)
+        if category != self.categories.ACCESSORY_MANIFEST:
+            primary_id_col = self.id_util.get_primary_id_column(self.category)
+            id_list = self.data_frame[primary_id_col].to_list()
+            for current_id in id_list:
+                current_id = str(current_id)
+                if not self.id_checker.is_valid_htan_id(primary_id_col, current_id):
+                    msg = f"Invalid {primary_id_col}:  {current_id}"
+                    msg = self._create_error_msg(msg)
+                    self.error_list.append(msg)
+                else:
+                    self.valid_primary_id_list.append(current_id)
 
         # Validate Parent IDs
         parent_id_col = self.id_util.get_parent_id_column(self.category)
         if parent_id_col is not None:
-            self.__check_adjacent_or_parent_ids(parent_id_col)
+            self.__check_adjacent_or_parent_ids(parent_id_col, self.category)
 
         # Validate Adjacent IDs
         adjacent_id_col = self.id_util.get_adjacent_id_column(self.category)
         if adjacent_id_col is not None:
-            self.__check_adjacent_or_parent_ids(adjacent_id_col)
+            self.__check_adjacent_or_parent_ids(adjacent_id_col, self.category)
 
-    def __check_adjacent_or_parent_ids(self, id_col):
+    def __check_adjacent_or_parent_ids(self, id_col, category):
         id_list = self.data_frame[id_col].to_list()
         if id_col == self.id_util.HTAN_PARENT_ID:
             target_check = self.id_util.HTAN_PARTICIPANT_ID
@@ -54,7 +57,18 @@ class MetaFileIdChecker:
             current_id_list = current_id_list.split(",")
             for current_id in current_id_list:
                 current_id = current_id.strip()
-                if not self.id_checker.is_valid_htan_id(target_check, current_id):
+                valid_id = False
+                # Special case for Level 2 data sets
+                if current_id.lower().startswith("not applicable") and (
+                    category == self.categories.BULK_WES_LEVEL_2
+                    or category == self.categories.BULK_RNA_SEQ_LEVEL_2
+                ):
+                    valid_id = True
+                else:
+                    valid_id = self.id_checker.is_valid_htan_id(
+                        target_check, current_id
+                    )
+                if not valid_id:
                     error_msg = f"Invalid {id_col}:  {current_id}"
                     msg = self._create_error_msg(error_msg)
                     self.error_list.append(msg)
