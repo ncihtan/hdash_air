23fff6fa09449a6812d6ad0d45a9b7775b6a5ad6
ecerami <cerami@gmail.com>
2025-08-04 11:30:17 -0400

    Fix broken unit tests

--- tests/test_s3_credentials.py
+++ tests/test_s3_credentials.py
@@ -6,17 +6,14 @@ from hdash.util.s3_credentials import S3Credentials
 
 def test_s3_credentials():
     """Test S3 Credentials."""
-    # Patch Airflow Variables.
-    # This is a recommended practiced described at:
-    # https://airflow.apache.org/docs/apache-airflow/2.0.2/best-practices.html
-    airflow_vars = {
-        "AIRFLOW_VAR_S3_ACCESS_KEY_ID": "key123",
-        "AIRFLOW_VAR_S3_SECRET_ACCESS_KEY": "secret123",
-        "AIRFLOW_VAR_S3_ENDPOINT_URL": "url123",
-        "AIRFLOW_VAR_S3_BUCKET_NAME": "hdash",
-        "AIRFLOW_VAR_S3_WEB_SITE_URL": "linode.com",
+    vars = {
+        "S3_ACCESS_KEY_ID": "key123",
+        "S3_SECRET_ACCESS_KEY": "secret123",
+        "S3_ENDPOINT_URL": "url123",
+        "S3_BUCKET_NAME": "hdash",
+        "S3_WEB_SITE_URL": "linode.com",
     }
-    with mock.patch.dict("os.environ", airflow_vars):
+    with mock.patch.dict("os.environ", vars):
         credentials = S3Credentials()
         assert credentials.access_key_id == "key123"
         assert credentials.secret_access_key == "secret123"
--- tests/test_synapse_credentials.py
+++ tests/test_synapse_credentials.py
@@ -6,14 +6,11 @@ from hdash.synapse.credentials import SynapseCredentials
 
 def test_synapse_credentials():
     """Test Synapse Credentials."""
-    # Patch Airflow Variables.
-    # This is a recommended practiced described at:
-    # https://airflow.apache.org/docs/apache-airflow/2.0.2/best-practices.html
-    airflow_vars = {
-        "AIRFLOW_VAR_SYNAPSE_USER": "user",
-        "AIRFLOW_VAR_SYNAPSE_PASSWORD": "password",
+    vars = {
+        "SYNAPSE_USER": "user",
+        "SYNAPSE_PASSWORD": "password",
     }
-    with mock.patch.dict("os.environ", airflow_vars):
+    with mock.patch.dict("os.environ", vars):
         credentials = SynapseCredentials()
         assert credentials.user_name == "user"
         assert credentials.password == "password"
